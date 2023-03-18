268e8c7eb17e0f921b498cbc7a34ada50e86f13c
Leonardo <parsdade@gmail.com>
2023-03-18 13:39:54 +0330

    Update x-ui.sh
    Fix Debian 8 Support

--- x-ui.sh
+++ x-ui.sh
@@ -53,8 +53,8 @@ elif [[ "${release}" == "fedora" ]]; then
     fi
 
 elif [[ "${release}" == "debian" ]]; then
-    if [[ ${os_version} -lt 10 ]]; then
-        echo -e "${red} Please use Debian 10 or higher ${plain}\n" && exit 1
+    if [[ ${os_version} -lt 8 ]]; then
+        echo -e "${red} Please use Debian 8 or higher ${plain}\n" && exit 1
     fi
 fi
 
