22e107c7465c087fa2a77be32a74b2602342ed21
Alireza Ahmadi <alireza7@gmail.com>
2023-03-19 11:56:42 +0100

    Update install.sh
    Fix centos7+ message

--- install.sh
+++ install.sh
@@ -48,7 +48,7 @@ os_version=$(grep -i version_id /etc/os-release | cut -d \" -f2 | cut -d . -f1)
 
 if [[ "${release}" == "centos" ]]; then
     if [[ ${os_version} -lt 7 ]]; then
-        echo -e "${red} Please use CentOS 8 or higher ${plain}\n" && exit 1
+        echo -e "${red} Please use CentOS 7 or higher ${plain}\n" && exit 1
     fi
 elif [[ "${release}" ==  "ubuntu" ]]; then
     if [[ ${os_version} -lt 18 ]]; then
