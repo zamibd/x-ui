6d404e4b27f43b2f7a5af8c03158d9fd77fd076b
Hamidreza Ghavami <hamid.r.gh.1998@gmail.com>
2023-04-14 04:44:04 +0430

    update get paths functions

--- xray/process.go
+++ xray/process.go
@@ -14,6 +14,7 @@ import (
 	"runtime"
 	"strings"
 	"time"
+	"x-ui/config"
 	"x-ui/util/common"
 
 	"github.com/Workiva/go-datastructures/queue"
@@ -29,19 +30,19 @@ func GetBinaryName() string {
 }
 
 func GetBinaryPath() string {
-	return "bin/" + GetBinaryName()
+	return config.GetBinFolderPath() + "/" + GetBinaryName()
 }
 
 func GetConfigPath() string {
-	return "bin/config.json"
+	return config.GetBinFolderPath() + "/config.json"
 }
 
 func GetGeositePath() string {
-	return "bin/geosite.dat"
+	return config.GetBinFolderPath() + "/geosite.dat"
 }
 
 func GetGeoipPath() string {
-	return "bin/geoip.dat"
+	return config.GetBinFolderPath() + "/geoip.dat"
 }
 
 func stopProcess(p *Process) {
