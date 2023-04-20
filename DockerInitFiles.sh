42abffdaef6ef1498e861c3edd8cc0b99351aa4f
Alireza Ahmadi <alireza7@gmail.com>
2023-04-20 10:36:46 +0200

    [docker] fix build for amd64

--- DockerInitFiles.sh
+++ DockerInitFiles.sh
@@ -10,8 +10,11 @@ else
     FNAME="amd64";
 fi
 mkdir -p build/bin
+cd build/bin
 wget "https://github.com/XTLS/Xray-core/releases/download/v1.8.1/Xray-linux-${ARCH}.zip"
-wget "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat" -o build/bin/geoip.dat
-wget "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat" -o build/bin/geosite.dat
-mv xray "build/bin/xray-linux-${FNAME}"
-mv main build/x-ui
+unzip "Xray-linux-${ARCH}.zip"
+rm -f "Xray-linux-${ARCH}.zip" geoip.dat geosite.dat
+mv xray "xray-linux-${FNAME}"
+wget "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
+wget "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
+cd ../../
--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 FROM golang:1.20-alpine AS builder
 WORKDIR /app
 ARG TARGETARCH 
-RUN apk --no-cache --update add build-base gcc wget
+RUN apk --no-cache --update add build-base gcc wget unzip
 COPY . .
-RUN env CGO_ENABLED=1 go build main.go
+RUN env CGO_ENABLED=1 go build -o build/x-ui main.go
 RUN ./DockerInitFiles.sh "$TARGETARCH"
 
 FROM alpine
