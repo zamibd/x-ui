1f5a785806ea65262ba161f095fdc7c1e8816506
Hamidreza Ghavami <70919649+hamid-gh98@users.noreply.github.com>
2023-05-16 01:22:37 +0430

    fix conflict

--- web/assets/js/util/common.js
+++ web/assets/js/util/common.js
@@ -5,109 +5,112 @@ const ONE_TB = ONE_GB * 1024;
 const ONE_PB = ONE_TB * 1024;
 
 function sizeFormat(size) {
-  if (size < ONE_KB) {
-    return size.toFixed(0) + " B";
-  } else if (size < ONE_MB) {
-    return (size / ONE_KB).toFixed(2) + " KB";
-  } else if (size < ONE_GB) {
-    return (size / ONE_MB).toFixed(2) + " MB";
-  } else if (size < ONE_TB) {
-    return (size / ONE_GB).toFixed(2) + " GB";
-  } else if (size < ONE_PB) {
-    return (size / ONE_TB).toFixed(2) + " TB";
-  } else {
-    return (size / ONE_PB).toFixed(2) + " PB";
-  }
+    if (size < ONE_KB) {
+        return size.toFixed(0) + " B";
+    } else if (size < ONE_MB) {
+        return (size / ONE_KB).toFixed(2) + " KB";
+    } else if (size < ONE_GB) {
+        return (size / ONE_MB).toFixed(2) + " MB";
+    } else if (size < ONE_TB) {
+        return (size / ONE_GB).toFixed(2) + " GB";
+    } else if (size < ONE_PB) {
+        return (size / ONE_TB).toFixed(2) + " TB";
+    } else {
+        return (size / ONE_PB).toFixed(2) + " PB";
+    }
 }
 
 function base64(str) {
-  return Base64.encode(str);
+    return Base64.encode(str);
 }
 
 function safeBase64(str) {
-  return base64(str).replace(/\+/g, "-").replace(/=/g, "").replace(/\//g, "_");
+    return base64(str)
+        .replace(/\+/g, '-')
+        .replace(/=/g, '')
+        .replace(/\//g, '_');
 }
 
 function formatSecond(second) {
-  if (second < 60) {
-    return second.toFixed(0) + " s";
-  } else if (second < 3600) {
-    return (second / 60).toFixed(0) + " m";
-  } else if (second < 3600 * 24) {
-    return (second / 3600).toFixed(0) + " h";
-  } else {
-    return (second / 3600 / 24).toFixed(0) + " d";
-  }
+    if (second < 60) {
+        return second.toFixed(0) + ' s';
+    } else if (second < 3600) {
+        return (second / 60).toFixed(0) + ' m';
+    } else if (second < 3600 * 24) {
+        return (second / 3600).toFixed(0) + ' h';
+    } else {
+        return (second / 3600 / 24).toFixed(0) + ' d';
+    }
 }
 
 function addZero(num) {
-  if (num < 10) {
-    return "0" + num;
-  } else {
-    return num;
-  }
+    if (num < 10) {
+        return "0" + num;
+    } else {
+        return num;
+    }
 }
 
 function toFixed(num, n) {
-  n = Math.pow(10, n);
-  return Math.round(num * n) / n;
+    n = Math.pow(10, n);
+    return Math.round(num * n) / n;
 }
 
 function debounce(fn, delay) {
-  var timeoutID = null;
-  return function () {
-    clearTimeout(timeoutID);
-    var args = arguments;
-    var that = this;
-    timeoutID = setTimeout(function () {
-      fn.apply(that, args);
-    }, delay);
-  };
+    var timeoutID = null;
+    return function () {
+        clearTimeout(timeoutID);
+        var args = arguments;
+        var that = this;
+        timeoutID = setTimeout(function () {
+            fn.apply(that, args);
+        }, delay);
+    };
 }
 
 function getCookie(cname) {
-  let name = cname + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(";");
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == " ") {
-      c = c.substring(1);
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == " ") {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
     }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
+    return "";
 }
 
 function setCookie(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-  let expires = "expires=" + d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    const d = new Date();
+    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
 function usageColor(data, threshold, total) {
-  switch (true) {
-    case data === null:
-      return "blue";
-    case total <= 0:
-      return "blue";
-    case data < total - threshold:
-      return "cyan";
-    case data < total:
-      return "orange";
-    default:
-      return "red";
-  }
+    switch (true) {
+        case data === null:
+            return "blue";
+        case total <= 0:
+            return "blue";
+        case data < total - threshold:
+            return "cyan";
+        case data < total:
+            return "orange";
+        default:
+            return "red";
+    }
 }
 
 function areAllItemsExist(array1, array2) {
-  for (let i = 0; i < array1.length; i++) {
-    if (!array2.includes(array1[i])) {
-      return false;
+    for (let i = 0; i < array1.length; i++) {
+        if (!array2.includes(array1[i])) {
+            return false;
+        }
     }
-  }
-  return true;
+    return true;
 }
