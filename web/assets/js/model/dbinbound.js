cfc11151d29dee39b906f30253ea1a9548a653a3
Alireza Ahmadi <alireza7@gmail.com>
2023-11-19 04:00:41 +0100

    Optimizing loading of script files

--- web/assets/js/model/dbinbound.js
+++ web/assets/js/model/dbinbound.js
@@ -1,30 +1,3 @@
-class User {
-
-    constructor() {
-        this.username = "";
-        this.password = "";
-    }
-}
-
-class Msg {
-
-    constructor(success, msg, obj) {
-        this.success = false;
-        this.msg = "";
-        this.obj = null;
-
-        if (success != null) {
-            this.success = success;
-        }
-        if (msg != null) {
-            this.msg = msg;
-        }
-        if (obj != null) {
-            this.obj = obj;
-        }
-    }
-}
-
 class DBInbound {
 
     constructor(data) {
@@ -172,50 +145,5 @@ class DBInbound {
 	get genInboundLinks() {
         const inbound = this.toInbound();
         return inbound.genInboundLinks(this.address, this.remark);
-    }
-}
-
-class AllSetting {
-
-    constructor(data) {
-        this.webListen = "";
-        this.webDomain = "";
-        this.webPort = 54321;
-        this.webCertFile = "";
-        this.webKeyFile = "";
-        this.webBasePath = "/";
-        this.sessionMaxAge = "";
-        this.pageSize = 0;
-        this.expireDiff = "";
-        this.trafficDiff = "";
-        this.tgBotEnable = false;
-        this.tgBotToken = "";
-        this.tgBotChatId = "";
-        this.tgRunTime = "@daily";
-        this.tgBotBackup = false;
-        this.tgBotLoginNotify = false;
-        this.tgCpu = "";
-        this.tgLang = "";
-        this.subEnable = false;
-        this.subListen = "";
-        this.subPort = "2096";
-        this.subPath = "/sub/";
-        this.subDomain = "";
-        this.subCertFile = "";
-        this.subKeyFile = "";
-        this.subUpdates = 0;
-        this.subEncrypt = true;
-        this.subShowInfo = false;
-
-        this.timeLocation = "Asia/Tehran";
-
-        if (data == null) {
-            return
-        }
-        ObjectUtil.cloneProps(this, data);
-    }
-
-    equals(other) {
-        return ObjectUtil.equals(this, other);
     }
 }
